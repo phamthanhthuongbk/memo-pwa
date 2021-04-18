282a9ff85f1c1733f8dd6de44fdd1d2200bf0aa7
Phạm Thanh Thường <phamthanhthuongbk@gmail.com>
2021-04-18 22:26:45 +0900

    deploy

---
+++ precache-manifest.fcdf4cff081c00528604582093097d98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a20a15471ba47abd5ed7",
+    "url": "/memo-pwa/css/app.30a4e642.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/memo-pwa/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "8eeb6520c93370683bfe3207289dae02",
+    "url": "/memo-pwa/index.html"
+  },
+  {
+    "revision": "a20a15471ba47abd5ed7",
+    "url": "/memo-pwa/js/app.f1b6628e.js"
+  },
+  {
+    "revision": "8722d0577d8dcdd91cf9",
+    "url": "/memo-pwa/js/chunk-vendors.3e81f15e.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/memo-pwa/logo.png"
+  },
+  {
+    "revision": "4f98f3cad2c489c68f387cf62c3aca96",
+    "url": "/memo-pwa/manifest.json"
+  }
+]);
